fa30ff9a6a3413e3abed84d38f4f2f6a55b0d6b0
milizeus <milizeus@MacBook-Air-2011-milizeus.local>
2013-03-12 15:10:33 +0100

    Initial Commit

---
+++ Computerorientierte Physik/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Computerorientierte Physik
+//
+//  Created by milizeus on 12.03.13.
+//  Copyright (c) 2013 uni-graz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
