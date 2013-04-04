271799a1c896454bf2d16c9449179f24308a0f3a
milizeus <milizeus@MacBook-Air-2011-milizeus.local>
2013-04-04 13:24:59 +0200

    bei 2. ODE fehlte ein "-" Energierechnung

--- 03_RK4_Doppelpendel/03_RK4_Doppelpendel.cpp
+++ 03_RK4_Doppelpendel/03_RK4_Doppelpendel.cpp
@@ -10,45 +10,25 @@
 #include <cmath>
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 
 /**************************************************************/
 /* Parameter                                                  */
 /**************************************************************/
-#define N_ODEs      4                   // number of ODE
-#define DELTA_T     0.001               // epsilonsize in s
-#define T_MAX       2                   // max for t
+#define         N_ODEs      4                   // number of ODE
+const double    DELTA_T    =0.001;               // epsilonsize in s
+const double    T_MAX      =60.0;                 // max for t
 
-const double    g=9.81;
-double          m=1;
-double          l=1;
+const double    g           =10;
+const double    m           =1.0;
+const double    l           =1.0;
 
-#define INITIAL_Y0  1.0                                     // phi 1
-#define INITIAL_Y1  1.0                                     // phi 2
-#define INITIAL_Y2  0.0                                     // p 1
-#define INITIAL_Y3  0.0                                     // p 2
+const double    INITIAL_Y0      =0.0;                             // phi 1
+const double    INITIAL_Y1      =0.0;                            // phi 2
+const double    INITIAL_Y2      =1.0;                             // p 1
+const double    INITIAL_Y3      =6.0;                             // p 2
 
-/**************************************************************/
-/* ODEs                                                       */
-/**************************************************************/
-double  F_ODE(double time, double y[], int i) {
-    
-    if (i==0) return(
-                     1/(m*l*l)*(y[2]-y[3]*cos(y[0]-y[1]))/(1+sin(y[0]-y[1])*sin(y[0]-y[1]))
-                    );
-    
-    if (i==1) return(
-                     1/(m*l*l)*(2*y[3]-y[2]*cos(y[0]-y[1]))/(1+sin(y[0]-y[1])*sin(y[0]-y[1]))
-                     );
-    
-    if (i==2) return(
-                     1/(m*l*l)*(y[2]*y[3]*sin(y[0]-y[1]))/(1+sin(y[0]-y[1])*sin(y[0]-y[1]))+1/(m*l*l)*(y[2]*y[2]+2*y[3]*y[3]-2*y[2]*y[3]*cos(y[0]-y[1]))/((1+sin(y[0]-y[1])*sin(y[0]-y[1]))*(1+sin(y[0]-y[1])*sin(y[0]-y[1])))*sin(y[0]-y[1])*cos(y[0]-y[1])-2*m*g*l*sin(y[0])
-                     );
-    
-    if (i==3) return(
-                     1/(m*l*l)*(y[2]*y[3]*sin(y[0]-y[1]))/(1+sin(y[0]-y[1])*sin(y[0]-y[1]))-1/(m*l*l)*(y[2]*y[2]+2*y[3]*y[3]-2*y[2]*y[3]*cos(y[0]-y[1]))/((1+sin(y[0]-y[1])*sin(y[0]-y[1]))*(1+sin(y[0]-y[1])*sin(y[0]-y[1])))*sin(y[0]-y[1])*cos(y[0]-y[1])-m*g*l*sin(y[1])
-                     );
-}
 
 /**************************************************************/
 /* main start                                                 */
@@ -65,22 +45,23 @@ int main(void){
     ofstream dataoutput("03_RK4_Doppelpendel.dat");
     dataoutput.setf(ios_base::scientific,ios_base::floatfield);
     
-    double time = 0.0, y[N_ODEs];
+    double time = 0.0, y[N_ODEs], NRG;
 
-    
+    void F_ODE(double time, double y[], int i);
     void runge4(double x, double y[], double epsilon);  // RK4
+    double energie ( double y[]);
     
     y[0]=INITIAL_Y0;                                    // initial Y[0]
     y[1]=INITIAL_Y1;                                    // initial Y[1]
     y[2]=INITIAL_Y2;                                    // initial Y[1]
     y[3]=INITIAL_Y3;                                    // initial Y[1]
-    dataoutput  << "# t" << " "  << "x" << " "  << "v" << endl;
-    dataoutput  << time << " "  << y[0] << " "  << y[1] << " "  << y[2] << " "  << y[3] << endl;
-    
+    dataoutput  << "# time" << "\t" << "phi1"<< "\t"<< "phi2"<<"\t"<<"p1"<<"\t"<<"p2" << endl;
+    dataoutput  << time << "\t"  << y[0] << "\t"  << y[1] << "\t"  << y[2] << "\t"  << y[3] << endl;
     
     for (time=0; time <= T_MAX+DELTA_T/5.0 ; time+=DELTA_T) {
         runge4(time, y, DELTA_T);
-        dataoutput  << time << " "  << y[0] << " "  << y[1] << " "  << y[2] << " "  << y[3] << endl;
+        NRG = energie(y);
+        dataoutput  << time << "\t"  << y[0] << "\t"  << y[1] << "\t"  << y[2] << "\t"  << y[3] << "\t"<< NRG << endl;
     }
     
     printf(" Datenfile 03_RK4_Doppelpendel.dat geschrieben");
@@ -90,13 +71,38 @@ int main(void){
     return 0;
 }
 
+
+
+/**************************************************************/
+/* ODEs                                                       */
+/**************************************************************/
+double  F_ODE(double time, double y[], int i) {
+    
+    if (i==0) return(
+                     1/(m*l*l)*(y[2]-y[3]*cos(y[0]-y[1]))/(1+sin(y[0]-y[1])*sin(y[0]-y[1]))
+                     );
+    
+    if (i==1) return(
+                     1/(m*l*l)*(2*y[3]-y[2]*cos(y[0]-y[1]))/(1+sin(y[0]-y[1])*sin(y[0]-y[1]))
+                     );
+    
+    if (i==2) return(
+                     -1/(m*l*l)*(y[2]*y[3]*sin(y[0]-y[1]))/(1+sin(y[0]-y[1])*sin(y[0]-y[1]))+1/(m*l*l)*(y[2]*y[2]+2*y[3]*y[3]-2*y[2]*y[3]*cos(y[0]-y[1]))/((1+sin(y[0]-y[1])*sin(y[0]-y[1]))*(1+sin(y[0]-y[1])*sin(y[0]-y[1])))*sin(y[0]-y[1])*cos(y[0]-y[1])-2*m*g*l*sin(y[0])
+                     );
+    
+    if (i==3) return(
+                     1/(m*l*l)*(y[2]*y[3]*sin(y[0]-y[1]))/(1+sin(y[0]-y[1])*sin(y[0]-y[1]))-1/(m*l*l)*(y[2]*y[2]+2*y[3]*y[3]-2*y[2]*y[3]*cos(y[0]-y[1]))/((1+sin(y[0]-y[1])*sin(y[0]-y[1]))*(1+sin(y[0]-y[1])*sin(y[0]-y[1])))*sin(y[0]-y[1])*cos(y[0]-y[1])-m*g*l*sin(y[1])
+                     );
+}
+
+
 /**************************************************************/
 /* RK4                                                        */
 /**************************************************************/
 void runge4(double time, double y[], double epsilon) {
-    double h=epsilon/2.0;                                              // the midpoint
-    double t1[N_ODEs], t2[N_ODEs], t3[N_ODEs];                         // temporary storage arrays
-    double k1[N_ODEs], k2[N_ODEs], k3[N_ODEs],k4[N_ODEs];              // for Runge-Kutta
+    double h=epsilon/2.0;
+    double t1[N_ODEs], t2[N_ODEs], t3[N_ODEs];                         // tmp
+    double k1[N_ODEs], k2[N_ODEs], k3[N_ODEs],k4[N_ODEs];
     int i;
     
     for (i=0; i < N_ODEs ; i++) t1[i] = y[i]+   0.5*(k1[i] = epsilon * F_ODE ( time            , y  , i ));
@@ -109,6 +115,14 @@ void runge4(double time, double y[], double epsilon) {
 }
 
 
+/**************************************************************/
+/* Energie                                                    */
+/**************************************************************/
+
+double energie ( double y[])
+{
+    return (y[2]*y[2]+2*y[3]*y[3]-2*y[2]*y[3]*cos(y[0]-y[1]))/(2*m*l*l)/(1+sin(y[0]-y[1])*sin(y[0]-y[1]))+m*g*l*(3-2*cos(y[0])-cos(y[1]));
+}
 
 
 
